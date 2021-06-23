d8bf53351b4d28dd1ad8754e4968a5b74c236038
Anne Cheylus <anne.cheylus@inserm.fr>
2021-06-23 22:09:15 +0200

    Règles à difficulté progressive, rapport d'abandons de règle et de persévérance dans l'ancienne règle

--- bx/BrixtonModif.js
+++ bx/BrixtonModif.js
@@ -1,31 +1,82 @@
 // Test de Brixton modifié
-// Adaptation pour l'Etude sur le déficit de transport de Créatine (Aurore Curie, HCL)
+// Adaptation pour le déficit en transporteur de Créatine (Aurore Curie, HCL/UCBL)
 // proposée par Eric Chabanat, UCBL
 // Code HTML par Anne Cheylus, CNRS
 
 
-// Liste des numeros des cercles (départ à 0 modulo 14)
+// Liste des numeros des cercles (départ à 0 modulo 14) : 0 1 2 .. 13
+// liste des règles pour passer d'un item au suivant
 var listeRegles = [
   {"increment": 1, "repetitions" :10},
   {"increment": -1, "repetitions" :10},
+  {"increment": -2, "repetitions" :7},
+  {"increment": 2, "repetitions" :7},
+  {"increment": -3, "repetitions" :7},
+  {"increment": 3, "repetitions" :7},
   {"increment": 7, "repetitions" :8},
   {"increment": -1, "repetitions" :5},
-  {"increment": 1, "repetitions" :5}
+  {"increment": 1, "repetitions" :5},
+  {"alternance": [-1,1], "repetitions" :5},
+  {"alternance": [-4,4], "repetitions" :5},
 ];
+var applique_regle = function(from,regle) {
+  var retval = -1;
+  if(regle.increment) {
+    retval = (14 + from + regle.increment) % 14 ;
+  } else if(regle.alternance) {
+    var first = regle.alternance.shift();
+    regle.alternance.push(first);
+    retval = (14 + from + first) % 14;
+  }
+  return(retval);
+};
+// Donner un nom à la règle
+var ajoute_nom = function(regle) {
+  if(regle.increment) {
+    regle.nom = (regle.increment > 0 ) ? "plus " + regle.increment : "minus " + (- regle.increment) ;
+  } else if(regle.alternance) {
+    regle.nom = "alt " + regle.alternance.join("/");
+  }
+};
+// Traduction des règles en liste de tests
 var listeTests = [];
 var from_item = 0;
 var to_item = 0;
 var predict_item = 0;
 var repetition = 0;
+var alt_items = [];
+var rule = "any";
+// Pour chaque règle,
 for (var i=0; i< listeRegles.length; i++) {
+  ajoute_nom(listeRegles[i]);
+  // pour chaque répétition
   for (var j=0; j< listeRegles[i].repetitions; j++) {
-    to_item = (14 + from_item + listeRegles[i].increment) % 14 ;
+    // l'item suivant est obtenu en appliquant la règle courante
+    to_item = applique_regle(from_item, listeRegles[i]) ;
     if ((j === 0) && (i>0) ) {
-      predict_item = (14 + from_item + listeRegles[i-1].increment) % 14 ;
+      // l'item prévisible est obtenu en appliquant la règle précédente au moment du switch
+      predict_item = applique_regle(from_item, listeRegles[i-1]) ;
       repetition = listeRegles[i-1].repetitions;
+      rule = listeRegles[i-1].nom;
     } else {
-      predict_item = to_item;
+      // si c'est la première alternance il faut 2 transitions pour établir la règle
+      if ((j===1) && listeRegles[i].alternance && (i>0) && listeRegles[i-1].increment) {
+        // une prédiction acceptable serait alors un incrément comme à la 1ère transition
+        predict_item  = applique_regle(from_item, {"increment": listeRegles[i].alternance[0]}) ;
+        rule="start_alt";
+      } else {
+        // sinon c'est l'item suivant
+        predict_item = to_item;
+        rule = ((i+j)===0) ? "start" :listeRegles[i].nom;
+      }
       repetition = j;
+    }
+    // liste des réponses alternatives pour les règles qui précèdent
+    // afin de séparer les réponses aberrantes des abandons de règle
+    alt_items = [];
+    for (var k=0; k<i; k++) {
+      // items obtenu en appliquant la règle k 
+      alt_items.push(applique_regle(from_item, listeRegles[k]));
     }
     listeTests.push({
       "title" : "Brixton modif " + i + "_" + j, 
@@ -33,7 +84,10 @@ for (var i=0; i< listeRegles.length; i++) {
       "to": to_item,
       "predict_item" : predict_item,
       "repetition" : repetition, 
-      "increment" : listeRegles[i].increment
+      "alt" : alt_items,
+      "alt_prev" : (i>0) ? alt_items[i-1] : undefined,
+      "increment" : listeRegles[i].increment,
+      "rule" : rule
     });
     from_item = to_item;
   }
@@ -41,7 +95,37 @@ for (var i=0; i< listeRegles.length; i++) {
 
 var currentTest=-1;
 var score = 0;
+var nb_perseverance = 0;
+var nb_abandon = 0;
+// test abandon : la regle courante a été adoptée au moins une fois
+// une règle proposée auparavant a été appliquée à sa place
+var test_abandon = function(listT,c,rep) {
+  var retval= false;
+  if (! listT[c].valid) {
+    // la regle courante a-t-elle déjà été adoptée ?
+    var listeRegleCourante = listT.filter((x,i) => (i<c) && (x.rule == listT[c].rule));
+    if (listeRegleCourante.some(x => x.valid)) {
+      // si la règle courante a déjà été adoptée, une règle précédente a-t-elle été appliquée ?
+      retval=listT[c].alt.some(x => ("c" + x == rep) );
+    }
+  }
+  return(retval);
+};
 
+// test perseverance : la regle courante n'a pas encore été appliquée
+// la règle proposée juste avant a été appliquée à sa place
+var test_perseverance = function(listT,c,rep) {
+  var retval= false;
+  if (! listT[c].valid) {
+    // la regle courante a-t-elle déjà été adoptée ?
+    var listeRegleCourante = listT.filter((x,i) => (i<c) && (x.rule == listT[c].rule));
+    if ( ! listeRegleCourante.some(x => x.valid)) {
+      // si la règle courante n'a pas encore été adoptée, la règle précédente a-t-elle été appliquée ?
+      retval = ("c" + listT[c].alt_prev == rep) ;
+    }
+  }
+  return(retval);
+};
 // enregistrer la réponse
 var setinfotrial = function(e) {
   console.log('setinfotrial ' + e.currentTarget.id);
@@ -49,18 +133,32 @@ var setinfotrial = function(e) {
   if(listeTests[currentTest].debut) {
     // infosTrial
     if (currentTest>=0 && currentTest < listeTests.length && e && e.currentTarget ) {
-      if (!listeTests[currentTest].hasOwnProperty('response')) {
+      if (!listeTests[currentTest].hasOwnProperty('valid')) {
+        // s'il n'y a pas encore de réponse enregistrée (1er essai)
+        listeTests[currentTest].response = e.currentTarget.id.replace(/^c/,'');
+        // voir si la réponse est juste ;
         // toujours compter la première réponse comme juste car on n'a pas encore de règle
-        listeTests[currentTest].response = (e.currentTarget.id == "c" + listeTests[currentTest].predict_item) ||
+        listeTests[currentTest].valid = (e.currentTarget.id == "c" + listeTests[currentTest].predict_item) ||
                                            (currentTest === 0);
         listeTests[currentTest].essais = 1;
         listeTests[currentTest].fin = new Date();
         listeTests[currentTest].RT = listeTests[currentTest].fin.getTime() - listeTests[currentTest].debut.getTime();
-        if (listeTests[currentTest].response) {
+        if (listeTests[currentTest].valid) {
           // score si bonne réponse au 1er essai
           score = score + 1;
         }
+        // abandons
+        listeTests[currentTest].abandon = test_abandon(listeTests,currentTest,e.currentTarget.id);
+        if (listeTests[currentTest].abandon) {
+          nb_abandon = nb_abandon + 1;
+        }
+        // perseverance
+        listeTests[currentTest].perseverance = test_perseverance(listeTests,currentTest,e.currentTarget.id);
+        if (listeTests[currentTest].perseverance) {
+          nb_perseverance = nb_perseverance + 1;
+        }
       } else {
+        // il y a déjà eu d'autre essais, incrémenter le compteur d'essais
         listeTests[currentTest].essais = listeTests[currentTest].essais + 1;
       }
       if(e.currentTarget.id == "c" + listeTests[currentTest].predict_item || currentTest === 0) {
@@ -117,7 +215,7 @@ var maj_graphique_cond = function(listeTests,type_match,cond) {
   if (type_match === ">=") {
     listeTestsCond = listeTests.filter(x => x.repetition >= cond);
   }
-  var error_rate_cond = listeTestsCond.reduce((prev,cur) => prev + !cur.response,0) * 100.0 / listeTestsCond.length;
+  var error_rate_cond = listeTestsCond.reduce((prev,cur) => prev + !cur.valid,0) * 100.0 / listeTestsCond.length;
   var median_rt = median(listeTestsCond,'RT');
   document.getElementById('er_' + cond).setAttribute('height', error_rate_cond * 4.83);
   document.getElementById('rt_' + cond).setAttribute('height', median_rt * 66/1000);
@@ -143,6 +241,8 @@ var endTest = function(e) {
     document.getElementById("allCSV").innerHTML = "Enregistrer test" + date_heure + ".csv";
     // rappel score sans le premier item
     document.getElementById("score").innerHTML = "Score : " + (score - 1) + " / " + (listeTests.length - 1);
+    document.getElementById("score").innerHTML += "<br>Abandons : " + nb_abandon ;
+    document.getElementById("score").innerHTML += "<br>Persévérances : " + nb_perseverance;
     document.getElementById("score").innerHTML += "<br>Début : " + listeTests[0].debut.toLocaleString();
     if(listeTests[listeTests.length-1].fin){
       document.getElementById("score").innerHTML += "<br>Fin : " + listeTests[listeTests.length-1].fin.toLocaleString();
@@ -154,17 +254,19 @@ var endTest = function(e) {
   var trialtable = document.getElementById("trialtable");
   var colnames = [
     "title","from",
-    "to","repetition","increment",
+    "to","repetition","rule",
     "debut","fin",
-    "essais","response","RT"
+    "essais","response",
+    "valid","abandon", "perseverance", 
+    "RT"
   ];
   trialtable.innerHTML="<thead><tr><th>"+ colnames.join("</th><th>")+"</th></tr></thead><tbody></tbody>";
   console.log(trialtable);
   document.getElementById("allCSV").href+= encodeURI(colnames.join(";")+"\n");
   for (var i=0; i< listeTests.length; i++) {
-    if(listeTests[i].hasOwnProperty("response")) {
+    if(listeTests[i].hasOwnProperty("valid")) {
       var infoligne=[];
-      colnames.forEach(x => infoligne.push(listeTests[i][x]));
+      colnames.forEach(x => infoligne.push(listeTests[i][x].toLocaleString ? listeTests[i][x].toLocaleString() : listeTests[i][x] ));
       trialtable.tBodies[0].innerHTML +="<tr><td>"+ infoligne.join("</td><td>") + "</td></tr>";
       document.getElementById("allCSV").href += encodeURI(infoligne.join(";") + "\n");
     }
