2fdd3a07f90581541aecfe450d57315002f853e3
Anne Cheylus <anne.cheylus@cnrs.fr>
2023-06-19 13:38:16 +0200

    first prototype for a high contrast scale

---
+++ scale/scale.js
@@ -0,0 +1,279 @@
+// Échelles à fort contraste pour Annie Moulin, CNRS
+// Code HTML par Anne Cheylus, CNRS
+
+
+// Liste des questions
+var listeQuestions = [
+  "Avez-vous confiance ?",
+  "Avez-vous encore confiance ?"
+];
+
+// temps maximum pour repondre : dix minutes (600000 ms)
+var maxwait = 600000;
+var checkTimeoutInterval = false;
+
+
+// Traduction des questions en liste de tests
+var listeTests = listeQuestions.map(q => ({'question': q}));
+
+var currentTest=-1;
+var score = 0;
+
+// enregistrer la réponse
+var setinfotrial = function(e) {
+  console.log('setinfotrial ' + e.currentTarget.id);
+  // seulement si le trial a debute
+  if(listeTests[currentTest].debut) {
+    // infosTrial
+    if (currentTest>=0 && currentTest < listeTests.length && e && e.currentTarget ) {
+      listeTests[currentTest].response = e.currentTarget.value;
+      score = score + listeTests[currentTest].response;
+      listeTests[currentTest].fin = new Date();
+      listeTests[currentTest].RT = listeTests[currentTest].fin.getTime() - listeTests[currentTest].debut.getTime();
+      gonext();
+    } 
+  } 
+};
+
+// stockage local
+var getCjtStorage = function() {
+    var myStorage = window.localStorage;
+    var cjt = myStorage.getItem('cjt');
+    if(!cjt) {
+      console.log("pas de stockage local : initialisation");
+      cjt = { ipad: Date.now().toString(36) + Math.random().toString(36).substr(2),
+              sessions: [],
+              testdata : []};
+    } else {
+      cjt = JSON.parse(cjt);
+      console.log("cjt =");
+      console.log(cjt);
+    }
+    return (cjt);
+};
+var setCjtStorage = function (cjt) {
+    var myStorage = window.localStorage;
+    myStorage.setItem('cjt',JSON.stringify(cjt));
+    console.log("cjt <-");
+    console.log(cjt);
+    return "0";
+};
+var clearCjtStorage = function() {
+  var myStorage = window.localStorage;
+  myStorage.removeItem('cjt');
+  return "0";
+};
+
+// fin du test
+var endTest = function(e) {
+  preventDefaultGestures = false;
+  document.getElementById("pausescreen").hidden = true;
+  document.getElementById("testscreen").hidden=true;
+  document.getElementById("endscreen").hidden=false;
+  if (checkTimeoutInterval) {
+    clearInterval(checkTimeoutInterval);
+    checkTimeoutInterval = false;
+  }
+  // Modifier le numero du sujet pour l'enregistrement
+  if(listeTests[0].debut) {
+    var date_heure = listeTests[0].debut.toISOString().substr(0,16).replace(/-|T|:/g,"");
+    var csv_name = listeTests[0].participant + "_" + document.title + "_" + date_heure + ".csv";
+    document.getElementById("allCSV").download=csv_name;
+    document.getElementById("allCSV").innerHTML = "Enregistrer " + csv_name;
+    // rappel score sans le premier item
+    document.getElementById("score").innerHTML = "Score : " + score  + " / " + (listeTests.length - 1);
+    document.getElementById("score").innerHTML += "<br>Participant : " + listeTests[0].participant;
+    document.getElementById("score").innerHTML += "<br>Début : " + listeTests[0].debut.toLocaleString();
+    if(listeTests[listeTests.length-1].fin){
+      document.getElementById("score").innerHTML += "<br>Fin : " + listeTests[listeTests.length-1].fin.toLocaleString();
+      var duree = new Date(listeTests[listeTests.length-1].fin.getTime() - listeTests[0].debut.getTime());
+      document.getElementById("score").innerHTML += "<br>Durée : " + duree.toISOString().substr(11, 8) ;
+    }
+  }
+  // table récapitulative
+  var trialtable = document.getElementById("trialtable");
+  var colnames = [
+    "participant","question","response","RT"
+  ];
+  trialtable.innerHTML="<thead><tr><th>"+ colnames.join("</th><th>")+"</th></tr></thead><tbody></tbody>";
+  console.log(trialtable);
+  document.getElementById("allCSV").href+= encodeURI(colnames.join(";")+"\n");
+  for (var i=0; i< listeTests.length; i++) {
+      var infoligne=[];
+      colnames.forEach(x => infoligne.push((listeTests[i][x] && listeTests[i][x].toLocaleString && (x!=="RT")) ? listeTests[i][x].toLocaleString() : listeTests[i][x] ));
+      trialtable.tBodies[0].innerHTML +="<tr><td>"+ infoligne.join("</td><td>") + "</td></tr>";
+      document.getElementById("allCSV").href += encodeURI(infoligne.join(";") + "\n");
+  }
+  if(listeTests[0].debut) {
+    // backup localStorage
+    var cjt = getCjtStorage();
+    cjt.sessions.push(document.location.href + '_' + listeTests[0].debut.toLocaleString());
+    cjt.testdata.push(listeTests);
+    setCjtStorage(cjt);
+    document.getElementById("cjtJSON").href += encodeURI(JSON.stringify(cjt) + "\n");
+  } 
+};
+
+// en cas d'erreur de chargement
+var errorload = function(e) {
+  listeTests[currentTest].response = 'load error for ' + e.currentTarget.id + " : could not load " + e.currentTarget.getAttribute("xlink:href");
+  endTest(e);
+};
+// en cas d'erreur autre
+var errormsg = function(e, msg) {
+  listeTests[currentTest].response = msg;
+  endTest(e);
+};
+// définition de la fonction pour aller au test suivant : gonext
+var gonext = function(e) {
+  console.log("gonext : " + currentTest);
+  
+  currentTest = currentTest+1;
+  if (currentTest < listeTests.length) {
+    // empecher zoom et defilements
+    preventDefaultGestures = true;
+    // modifier le titre
+    var title = document.getElementById("title");
+    title.innerHTML = "Score : " + score;
+    title.hidden = true;
+    document.getElementById("scale").value = 5;
+    document.getElementById("question").innerHTML = listeTests[currentTest].question;
+    // c'est parti
+    listeTests[currentTest].debut=new Date();
+  } else {
+    // c'est fini
+    endTest();
+  } 
+};
+
+// Reprendre (apres interruption prolongee)
+var reprendre = function() {
+  currentTest = currentTest - 1;
+  document.getElementById("pausescreen").hidden = true;
+  document.getElementById("testscreen").hidden = false;
+  gonext();
+};
+
+// Mettre en pause (si interruption prolongee)
+var pause = function() {
+  document.getElementById("reprendre").addEventListener('click', reprendre, {once: true, capture: true});
+  document.getElementById("terminer").addEventListener('click', endTest, {once: true, capture: true});
+  document.getElementById("testscreen").hidden = true;
+  document.getElementById("pausescreen").hidden = false;
+};
+
+// Verifier que le sujet est bien actif (absence d'interruption prolongee)
+var checkTimeout = function() {
+  if ( !document.getElementById("testscreen").hidden ) {
+    if (currentTest>=0 && currentTest < listeTests.length && listeTests[currentTest].debut ) {
+      datenow =  new Date();
+      elapsed = datenow.getTime() - listeTests[currentTest].debut.getTime();
+      if (elapsed > maxwait) {
+        pause();
+      }
+    } 
+  }
+};
+
+
+// lire le code participant et commencer l'essai
+var commencer = function() {
+  listeTests.forEach(x => x.participant = document.getElementById("participant_code").value);
+  console.log(listeTests[0].participant);
+  // masquer le clavier et le formulaire, afficher le test
+  document.activeElement.blur();
+  document.getElementById("infosuj").hidden = true;
+  document.getElementById("testscreen").hidden = false;
+  // verifier l'absence d'interruption prolongee
+  checkTimeoutInterval = setInterval(checkTimeout,1000);
+  // premier trial
+  gonext();
+  // renvoyer false pour eviter le rechargement de la page a la soumission du formulaire
+  return(false); 
+};
+
+// mise a jour du cache pour les ipads (obsolète mais utile si ancienne version installée sur l'ipad)
+var maj_cache = function(e) {
+    window.applicationCache.swapCache();
+    endTest(e);
+};
+
+/*
+ * Teste si un sous-test est demandé dans l'adresse
+ */
+var testeAdressSousTest = function(adress) {
+    document.title = "Échelle"; 
+    if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+        // demander la version
+        navigator.serviceWorker.controller.postMessage('version');
+    } else {
+        document.getElementById("title").insertAdjacentText("afterend", "Absence de contrôle du cache\n");
+    }
+};
+
+/*
+ * Tests infos codees dans l'adresse et le protocole
+ */
+var testeAdress = function() {
+    var protocol=document.location.protocol;
+    var adress=document.location.search;
+
+    testeAdressSousTest(adress);
+};
+// s'il n'y a pas de mise a jour, charger le test
+var onCacheOK = function() {
+  // si le sous-test est specifie, adapter la liste des tests
+  testeAdress();
+  // lorsqu'on clique dessus enregistrer la réponse
+  if( 'ontouchstart' in window) {
+    document.getElementById("scale").addEventListener('touchstart', setinfotrial, true);
+  } else {
+    document.getElementById("scale").addEventListener('click', setinfotrial, true);
+  }
+  commencer();
+}
+// log message from serviceWorker
+var logMsg = function(evt) {
+  console.log(evt);
+  if (evt.data) {
+    if (evt.data.indexOf('version:')==0) {
+      document.getElementById("title").innerHTML = document.getElementById("title").innerHTML.replace(/\( version: [^\)]*\)|$/, " ( "+ evt.data + " )");
+    } else {
+      document.getElementById("title").insertAdjacentText("afterend",evt.data + "\n");
+    }
+  } else if (evt.type == "controllerchange") {
+    if (navigator.serviceWorker.controller) {
+        // demander la version
+        navigator.serviceWorker.controller.postMessage('version');
+    } else {
+      document.getElementById("title").insertAdjacentText("afterend", 
+      "Un changement de contrôleur est intervenu, mais le contrôleur n'est pas défini.\n");
+    }
+    // en cas de changement de contrôleur, s'il n'y a pas un test en cours, mieux vaut recharger la page
+    if (currentTest <0) {
+      document.getElementById("infosuj").hidden = true;
+      endTest(evt);
+    }
+    
+  } else {
+    document.getElementById("title").insertAdjacentText("afterend", "! ! ! ! ! "+ JSON.stringify(evt) + " ! ! ! ! !\n");
+  }
+};
+
+// cette fonction (window.onload) s'exécute lorsque la page est chargée
+window.onload = function() {
+  // Mettre a jour l'adresse pour nextsuj
+  document.getElementById("nextsuj").href = document.location.href;
+  // gestion du cache
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.addEventListener('message', logMsg);
+    navigator.serviceWorker.addEventListener('error', logMsg);
+    navigator.serviceWorker.addEventListener('controllerchange', logMsg);
+    // skip le cache HTTP du navigateur, c'est le sw qui gerera le cache
+    navigator.serviceWorker.register('sw.js',{updateViaCache: 'none'}).then(onCacheOK);
+  } else {
+    document.getElementById("title").insertAdjacentText("afterend", "! ! ! ! ! Pas d'utilisation hors ligne possible ! ! ! ! !\n");
+    onCacheOK();
+  }
+};
+
---
+++ scale/sw.js
@@ -0,0 +1,108 @@
+console.log('[Service Worker] registering');
+appName = 'scale';
+appVersion = '0.1';
+cacheName =  appName + '-v' + appVersion ;
+alllist = [ 'Style.css',
+            'index.html',
+            'scale.js',
+            'echelle.svg'];
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installation');
+    // D'abord on va vider le cache avec les anciennes versions  
+    // liste des versions a conserver
+    var cachesToKeep = [cacheName, 'immuable'];
+  
+    e.waitUntil( (async() => {
+      try {
+        const keyList = await caches.keys();
+        console.log('[Service Worker] Nettoyage du cache');
+        await Promise.all(keyList.map(function(key) {
+            if (cachesToKeep.indexOf(key) === -1 && key.indexOf(appName + "-v") === 0) {
+              console.log('[Service Worker] Suppression de ', key);
+              return caches.delete(key);
+            }
+        }));
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Mise en cache globale: app shell et contenu');
+        await Promise.all(alllist.map(async(key) => {
+          console.log('[Service Worker] Mise en cache de',key);
+          // le cache du sw est different du cache HTTP
+          // cache: 'no-store' contourne le cache HTTP et permet d'acceder au serveur
+          // cache: 'reload' met a jour le cache HTTP en interrogeant le serveur
+          // par defaut s'il y a une donnee fraiche en cache HTTP 
+          // le serveur n'est pas interroge
+          const fetchResponse = await fetch(key, {
+            mode:'no-cors',
+            cache:'no-store'
+          });
+          if(fetchResponse && fetchResponse.ok) {
+            console.log('fetchResponse: ', fetchResponse.url);
+            return cache.put(fetchResponse.url, fetchResponse.clone());
+          } else {
+            throw new TypeError('error fetching' + key);
+          }
+        }));
+        console.log('[Service Worker] skip waiting');
+        await self.skipWaiting();
+        return true;
+      } catch (error) {
+        console.log('Install failed: ', error);
+        return error;
+      }
+    })());
+});
+self.addEventListener('activate',(e) => {
+  console.log('[Service Worker] Activation');
+  e.waitUntil((async() => {
+    try {
+      console.log('[Service Worker] claim clients');
+      await self.clients.claim();
+      console.log('[Service Worker] inform clients');
+      const clientList = await self.clients.matchAll();
+      return Promise.all(clientList.map((c) => c.postMessage(cacheName + ' activated')));
+    } catch(error) {
+      console.log('Activate failed: ',error);
+      return error;
+    }
+  })());
+});
+self.addEventListener('message',(e) => {
+  console.log('[Service Worker] message received',e.data);
+  if (e.data === "version") {
+    e.source.postMessage('version: ' + cacheName);
+  }
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async() => {
+    const cache = await caches.open(cacheName);
+    try {
+        // d'abord voir sur le reseau
+        // le cache du sw est different du cache HTTP
+        // cache: 'no-store' contourne le cache HTTP et permet d'acceder au serveur
+        // cache: 'reload' met a jour le cache HTTP en interrogeant le serveur
+        // par defaut s'il y a une donnee fraiche en cache HTTP le serveur n'est pas interroge
+        const fetchResponse = await fetch(event.request, {
+          mode:'no-cors',
+          cache:'no-store'
+        });
+        if(fetchResponse) {
+            console.log('fetchResponse: ', event.request.url);
+            await cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+        }
+    } catch(error1) {
+      console.log('Fetch failed: ', error1);
+      try {
+        // en cas d'echec interroger le cache du sw
+        const cachedResponse = await cache.match(event.request);
+        if(cachedResponse) {
+            console.log('cachedResponse: ', event.request.url);
+            return cachedResponse;
+        }
+      } catch (error) {
+        const cachedResponse = await cache.match('offline.html');
+        return cachedResponse;
+      }
+    }
+  })());
+});
