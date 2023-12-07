92f3fb3f12c350b19c0ff693bdd33ec71b15a258
Anne Cheylus <anne.cheylus@cnrs.fr>
2023-12-07 15:57:17 +0100

    test adaptation grand écran

--- scale/sw.js
+++ scale/sw.js
@@ -1,6 +1,6 @@
 console.log('[Service Worker] registering');
 appName = 'scale';
-appVersion = '0.3.14';
+appVersion = '0.3.15';
 cacheName =  appName + '-v' + appVersion ;
 alllist = [ 'Style.css',
             'index.html',
