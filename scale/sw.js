91c3fce2d6b5d354f20e0b3b943b5b960e3ccc2d
Anne Cheylus <anne.cheylus@cnrs.fr>
2023-12-07 13:56:36 +0100

    test css pour ancien iPadOS

--- scale/sw.js
+++ scale/sw.js
@@ -1,6 +1,6 @@
 console.log('[Service Worker] registering');
 appName = 'scale';
-appVersion = '0.3.12';
+appVersion = '0.3.13';
 cacheName =  appName + '-v' + appVersion ;
 alllist = [ 'Style.css',
             'index.html',
